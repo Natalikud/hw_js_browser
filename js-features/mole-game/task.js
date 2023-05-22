146f2fee37113cbf059f6d62819a7fa9f11b5f04
Natalikud2 <Reimova091@mail.ru>
2023-05-22 09:15:38 +0800

    Version 2

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,26 +1,30 @@
 function getHole(index) {
-    let holeId = document.getElementById(`hole${index}`);
-    return holeId;
-  }
-  let deadCount = 0;
-  let lostCount = 0;
-  
-  for (let i = 1; i <= 9; i++) {
-    getHole(i).onclick = function() {
-      if (getHole(i).classList.contains('hole_has-mole')) {
-        deadCount++;
-        document.getElementById('dead').textContent = deadCount;
-        if (deadCount === 10) {
-          alert('Победа!');
-          deadCount = 0;
-        }
-      } else {
-        lostCount++;
-        document.getElementById('lost').textContent = lostCount;
-          if (lostCount === 5) {
-            alert('Поражение!');
-            lostCount = 0;
-          }
+  let holeId = document.getElementById(`hole${index}`);
+  return holeId;
+}
+let deadCount = 0;
+let lostCount = 0;
+
+for (let i = 1; i <= 9; i++) {
+  getHole(i).onclick = function() {
+    if (getHole(i).classList.contains('hole_has-mole')) {
+      deadCount++;
+      document.getElementById('dead').textContent = deadCount;
+      if (deadCount === 10) {
+        deadCount = 0;
+        lostCount = 0;
+        alert('Победа!');
+        
       }
+    } else {
+      lostCount++;
+      document.getElementById('lost').textContent = lostCount;
+        if (lostCount === 5) {
+          deadCount = 0;
+          lostCount = 0;
+          alert('Поражение!');
+          
+        }
     }
   }
+}
