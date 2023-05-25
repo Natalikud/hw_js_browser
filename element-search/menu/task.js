180024569c383556ce6b4cd07ffbf11b0262d270
Natalikud2 <Reimova091@mail.ru>
2023-05-25 14:52:10 +0800

    Homework 2. Version 2

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,17 +1,13 @@
-//Используя цикл зарегистрируйте обработчики события click на элементах с классом menu__link
-//Найдите меню рядом со ссылкой. Если оно есть, переключите у него класс menu_active
-//Запрещайте переход по ссылке для тех, 
-//что имеют вложенное меню. Остальные пункты меню должны без помех переводить пользователя на соответствующие страницы.
-
-const link = document.querySelectorAll('.menu__link');
-
-for (let i = 0; i < link.length; i++) {  
-    let parent2 = link[i].closest('ul');
-    let child = link[i].closest("li").querySelector('ul');
-    link[i].onclick = () => {
-        if (parent2.className == "menu menu_main" && child.className == "menu menu_sub") {
-            child.className += " menu_active";
-            return false;
+const subMenu = document.getElementsByClassName("menu menu_sub");
+    
+for(let i=0; i < subMenu.length; i++) {
+    let link = subMenu[i].closest(".menu__item").querySelector(".menu__link");
+        
+    link.onclick = () => {
+        if (subMenu[i].className == "menu menu_sub menu_active") {
+            subMenu[i].className = "menu menu_sub";
+        } else {subMenu[i].className += " menu_active";
         };
-    };      
+        return false;
+    };
 };
--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,5 +1,5 @@
 //коллекция слайдов {массив}
-const sliders = document.getElementsByClassName('slider__item');
+const sliders = Array.from(document.getElementsByClassName('slider__item'));
 
 //коллекция кнопок - влево, вправо {массив}
 const navigations = document.getElementsByClassName('slider__arrow');
@@ -9,13 +9,24 @@ const next = navigations[1];
 
 //сохранить в переменную номер активного слайда 
 //( при смене слайда, значение будет меняться)
-const number = function() {
-    for (i=0; i < sliders.length; i++) {
-        if (sliders[i].className.includes('slider__item slider__item_active')) {
-            return i;
-        };
-    };
+
+//const number = function() {
+//    for (i=0; i < sliders.length; i++) {
+//        if (sliders[i].className.includes('slider__item slider__item_active')) {
+//           return i;
+//        };
+//    };
+//};
+//console.log(number())
+
+//в доработке - для number лучше использовать метод findIndex:
+function active(el) {
+    return el.className == 'slider__item slider__item_active'
 };
+const number = function() {
+    return sliders.findIndex(active)
+};
+    
 
 function slider(index) {
     for (let i=0; i < sliders.length; i++) {
